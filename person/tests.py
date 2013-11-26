e9a4b32b720acf470fa73fb8dc214bbd40e46361
Kostya Shish <shish.kostya@gmail.com>
2013-11-26 13:56:57 +0200

    fixes

--- person/tests.py
+++ person/tests.py
@@ -7,19 +7,22 @@ from person.models import Person
 class PersonTest(TestCase):
 
     def setUp(self):
-        self.person = Person.objects.create(name='John',
-                                            surname='Smith',
-                                            bithdate='1991-01-09')
+        Person.objects.create(name='John',
+                              surname='Smith',
+                              bithdate='1991-01-09')
         self.user = User.objects.create_user('john', 'john@mail.com', 'password')
 
     def test_main_page_view(self):
         response = self.client.get('/')
-        person = response.context['person']
+        context_person = response.context['person']
 
-        self.assertEqual(person.pk, 1)
-        self.assertIn(self.person.name, response.content)
-        self.assertIn(self.person.surname, response.content)
-        self.assertIn(formats.date_format(person.bithdate, "DATE_FORMAT"), response.content)
+        self.assertEqual(context_person.pk, 1)
+
+        person = Person.objects.get(pk=1)
+
+        self.assertContains(response, person.name)
+        self.assertContains(response, person.surname)
+        self.assertContains(response, formats.date_format(person.bithdate, "DATE_FORMAT"))
 
     def test_save_main_page_data(self):
         self.client.login(username='john', password='password')
