265bf4b7c6b4940d511adbbbf4417293857e9f99
Kostya Shish <shish.kostya@gmail.com>
2013-11-29 09:44:39 +0200

    ticket:10 unit tests

--- utils/tests.py
+++ utils/tests.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.template import Template, Context
 from django.contrib.auth.models import User
-from utils.models import Request
+from utils.models import Request, ModelsLog, ACTION_CREATE,  ACTION_UPDATE, ACTION_DELETE
 from utils.views import RequestsView
 
 
@@ -39,3 +39,29 @@ class UtilsTest(TestCase):
         change_link = reverse('admin:auth_user_change', args=(user.pk,))
 
         self.assertEqual(rendered, '<a href="%s">(admin)</a>' % change_link)
+
+    def test_models_log_object(self):
+        request = Request.objects.create(path='test')
+
+        self.assertEqual(ModelsLog.objects.all().count(), 1)
+
+        log_entry = ModelsLog.objects.get(pk=1)
+        self.assertEqual(log_entry.action, ACTION_CREATE)
+        self.assertEqual(log_entry.model, Request._meta.object_name)
+
+        request.path = 'test2'
+        request.save()
+
+        self.assertEqual(ModelsLog.objects.all().count(), 2)
+
+        log_entry = ModelsLog.objects.get(pk=2)
+        self.assertEqual(log_entry.action, ACTION_UPDATE)
+        self.assertEqual(log_entry.model, Request._meta.object_name)
+
+        request.delete()
+
+        self.assertEqual(ModelsLog.objects.all().count(), 3)
+
+        log_entry = ModelsLog.objects.get(pk=3)
+        self.assertEqual(log_entry.action, ACTION_DELETE)
+        self.assertEqual(log_entry.model, Request._meta.object_name)
